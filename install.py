cba4561741b385312aeea1651945b97584110278
Takeshi Kanmae <tkanmae@gmail.com>
2013-01-09 22:00:25 -0900

    Improve doc

--- install.py
+++ install.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 """Install all the dotfiles.
 
-Create symlinks in HOME pointing to actual files in TOP.  If any of the dotfiles
-already exist in HOME and they are not symlinks, they will be saved as their
-filenames appended with a ".orig".
+Create symlinks in `HOME` pointing to dotfiles in `TOP`.  Files/directories that
+have names starting with '.' are considered to be dotfiles.  Any dotfiles to be
+ignored from the installation must be listed in a global variable `EXCLUDE`.  If
+any of the dotfiles already exist in `HOME` and they are not symlinks, they will
+be saved as their file names appended with a ".orig".
 """
 import argparse
 import glob
