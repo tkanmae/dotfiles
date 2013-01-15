0a7a23d2f8407dadad8ee9d91fd90a45df34bfbd
Takeshi Kanmae <tkanmae@gmail.com>
2013-01-14 16:59:34 -0900

    Remove update-submodules.sh

--- update-submodules.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# ------------------------------------------------------------------------------
-# Update submodules
-# ------------------------------------------------------------------------------
-git submodule foreach --recursive 'git pull origin master'
-# git submodule update --recursive
