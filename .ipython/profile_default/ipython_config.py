a8dae4a3a2eb7bac888e46316701fc2c7d0c560f
Takeshi Kanmae <tkanmae@gmail.com>
2013-09-06 19:29:52 -0800

    ipython: Use msgpack for packer/unpacker

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -6,3 +6,11 @@ c.InteractiveShellApp.exec_lines = [
 c.InteractiveShellApp.extensions = ['autoreload']
 c.TerminalInteractiveShell.colors = 'Linux'
 c.PromptManager.in_template = '{color.Yellow}\Y5\n{color.Green}In [\\#]: '
+
+try:
+    import msgpack
+except ImportError:
+    pass
+else:
+    c.Session.packer = 'msgpack.packb'
+    c.Session.unpacker = 'msgpack.unpackb'
