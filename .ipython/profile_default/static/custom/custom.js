680ddcb4863ff3c6d504291b6f3053527e1575bc
Takeshi Kanmae <tkanmae@gmail.com>
2014-05-25 12:45:34 -0800

    ipython: Enable Vim setting

--- .ipython/profile_default/static/custom/custom.js
+++ .ipython/profile_default/static/custom/custom.js
@@ -1,11 +1,35 @@
 "use strict";
 
+// Use Vim key bindings.
+$.getScript("/static/components/codemirror/keymap/vim.js", function() {
+  if (IPython.Cell) {
+    IPython.Cell.options_default.cm_config.keyMap = "vim";
+  }
+});
+
+
+// Enable line wrapping in CodeCell.
+if (IPython.CodeCell) {
+  IPython.CodeCell.options_default.cm_config.lineWrapping = true;
+}
+
+
 $([IPython.events]).on('app_initialized.NotebookApp', function() {
   if (window.marked) {
     window.marked.setOptions({smartypants: true});
   }
-});
 
-if (IPython.CodeCell) {
-  IPython.CodeCell.options_default.cm_config.lineWrapping = true;
-}
+  // Handle <ESC> in the way Vim handles it.
+  IPython.keyboard_manager.edit_shortcuts.add_shortcut('esc', function(event) {
+    var cm = IPython.notebook.get_selected_cell().code_mirror;
+
+    if (cm.state.vim) {
+      if (cm.state.vim.insertMode) {
+        CodeMirror.keyMap['vim-insert']['Esc'](cm);
+        return false;
+      }
+    }
+    IPython.notebook.command_mode();
+    return false;
+  });
+});
