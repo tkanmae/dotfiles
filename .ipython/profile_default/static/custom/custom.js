78888ead4f8b1b168e08219597a93032747320ea
Takeshi Kanmae <tkanmae@gmail.com>
2014-05-25 12:45:58 -0800

    ipython: Load a CM add-on

--- .ipython/profile_default/static/custom/custom.js
+++ .ipython/profile_default/static/custom/custom.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Load CodeMirror Add-ons.
+$.getScript("/static/components/codemirror/addons/edit/closebrackets.js");
+
 // Use Vim key bindings.
 $.getScript("/static/components/codemirror/keymap/vim.js", function() {
   if (IPython.Cell) {
