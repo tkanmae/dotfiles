9f14e5a931544703675ba4eaf7526610d746c0ae
Takeshi Kanmae <tkanmae@gmail.com>
2014-05-25 12:39:53 -0800

    ipython: Use symlinks

--- .ipython/profile_science/static/custom/custom.js
+++ .ipython/profile_science/static/custom/custom.js
@@ -1,11 +1,1 @@
-"use strict";
-
-$([IPython.events]).on('app_initialized.NotebookApp', function() {
-  if (window.marked) {
-    window.marked.setOptions({smartypants: true});
-  }
-});
-
-if (IPython.CodeCell) {
-  IPython.CodeCell.options_default.cm_config.lineWrapping = true;
-}
+../../../profile_default/static/custom/custom.js
