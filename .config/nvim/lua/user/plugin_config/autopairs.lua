35ee24d4017653a4695add6431e990ed40905977
Takeshi Kanmae <tkanmae@gmail.com>
2022-10-07 08:53:57 +0900

    vim: Make nvim-autopairs aware of tree-sitter

--- .config/nvim/lua/user/plugin_config/autopairs.lua
+++ .config/nvim/lua/user/plugin_config/autopairs.lua
@@ -5,5 +5,7 @@ if not ok then
 end
 
 autopairs.setup({
+  check_ts = true,
+  ts_config = { lua = { 'string' }, javascript = { 'string', 'template_string' } },
   disable_filetype = { 'TelescopePrompt', 'vim' },
 })
