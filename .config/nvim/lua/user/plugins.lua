b79adf1b596b01e43a80bdca0ca8c395cd8e6fc9
Takeshi Kanmae <tkanmae@gmail.com>
2022-12-08 20:33:15 +0900

    vim: Lazy-load gitsigns.nvim

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -120,7 +120,7 @@ return require('packer').startup(function(use)
   -- Git
   use({
     'lewis6991/gitsigns.nvim',
-    event = 'BufEnter',
+    event = { 'FocusLost', 'CursorHold' },
     config = [[require("user.plugin_config.gitsigns")]],
   })
 
