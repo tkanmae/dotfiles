96c085ea9e2a075ba1137ba0d707412f86c6eb41
Takeshi Kanmae <tkanmae@gmail.com>
2023-02-25 13:24:09 +0900

    vim: Map <C-h> to delete a pair

--- .config/nvim/lua/user/plugin_config/autopairs.lua
+++ .config/nvim/lua/user/plugin_config/autopairs.lua
@@ -8,4 +8,5 @@ autopairs.setup({
   check_ts = true,
   ts_config = { lua = { 'string' }, javascript = { 'string', 'template_string' } },
   disable_filetype = { 'TelescopePrompt', 'vim' },
+  map_c_h = true,
 })
