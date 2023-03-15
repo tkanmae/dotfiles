034875591e7cfecce417ac7452f2356d9d6b36c2
Takeshi Kanmae <tkanmae@gmail.com>
2023-03-15 22:13:42 +0900

    vim: Enable incremental selection

--- .config/nvim/lua/user/plugin_config/treesitter.lua
+++ .config/nvim/lua/user/plugin_config/treesitter.lua
@@ -64,4 +64,13 @@ treesitter.setup({
     },
   },
   matchup = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<C-space>',
+      node_incremental = '<C-space>',
+      scope_incremental = '<nop>',
+      node_decremental = '<bs>',
+    },
+  },
 })
