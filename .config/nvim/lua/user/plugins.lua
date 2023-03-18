63193746aa679966b98cc3f06e0685a7cb666394
Takeshi Kanmae <tkanmae@gmail.com>
2023-03-18 18:25:31 +0900

    vim: Silence warning messages from Lua LS

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -413,10 +413,12 @@ require('lazy').setup({
     'stevearc/dressing.nvim',
     lazy = true,
     init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
       vim.ui.select = function(...)
         require('lazy').load({ plugins = { 'dressing.nvim' } })
         return vim.ui.select(...)
       end
+      ---@diagnostic disable-next-line: duplicate-set-field
       vim.ui.input = function(...)
         require('lazy').load({ plugins = { 'dressing.nvim' } })
         return vim.ui.input(...)
