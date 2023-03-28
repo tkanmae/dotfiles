a14695f0216448e71a56481f79826359f30d69f8
Takeshi Kanmae <tkanmae@gmail.com>
2023-03-28 22:43:09 +0900

    vim: Use Ruff instead of Flake8

--- .config/nvim/lua/user/plugin_config/null-ls.lua
+++ .config/nvim/lua/user/plugin_config/null-ls.lua
@@ -28,7 +28,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettier,
     -- Python
-    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.ruff,
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.isort,
     -- Lua
