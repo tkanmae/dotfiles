a21eeb9097177633ab0455a8f0287ea3d166bb61
Takeshi Kanmae <tkanmae@gmail.com>
2023-04-03 09:31:21 +0900

    Re-format install.py with Black

--- install.py
+++ install.py
@@ -12,8 +12,7 @@ import argparse
 import glob
 import os
 
-
-HOME = os.path.expandvars('$HOME')
+HOME = os.path.expandvars("$HOME")
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -23,22 +22,25 @@ def is_exe(filename):
 
 def create_symlink(src, dst):
     if not os.path.exists(src):
-        raise ValueError('File not found: %s' % src)
+        raise ValueError("File not found: %s" % src)
 
     if os.path.islink(dst):
         os.remove(dst)
     elif os.path.exists(dst):
-        os.rename(dst, dst + '.orig')
+        os.rename(dst, dst + ".orig")
     os.symlink(src, dst)
 
 
 def create_symlinks(srcs, dsts, dryrun):
     num = max([len(os.path.relpath(_, HOME)) for _ in dsts])
-    fmt = '{{0:<{0}s}} -> {{1}}'.format(num + 2)
+    fmt = "{{0:<{0}s}} -> {{1}}".format(num + 2)
 
     def display(src, dst):
-        print(fmt.format('~/' + os.path.relpath(dst, HOME),
-                         '~/' + os.path.relpath(src, HOME)))
+        print(
+            fmt.format(
+                "~/" + os.path.relpath(dst, HOME), "~/" + os.path.relpath(src, HOME)
+            )
+        )
 
     for src, dst in zip(srcs, dsts):
         display(src, dst)
@@ -53,7 +55,7 @@ def install_dotfiles(dotfiles, dryrun):
 
 
 def install_binfiles(binfiles, dryrun):
-    dst_dir = os.path.join(HOME, '.local/bin')
+    dst_dir = os.path.join(HOME, ".local/bin")
     if not os.path.isdir(dst_dir):
         os.makedirs(dst_dir)
     srcs = [os.path.join(HERE, f) for f in binfiles]
@@ -65,28 +67,33 @@ def setup_argparser():
     parser = argparse.ArgumentParser()
 
     # Optional arguments
-    parser.add_argument('-n', '--dry-run', dest='dryrun',
-                        action='store_true', default=False,
-                        help="perform a trial run")
+    parser.add_argument(
+        "-n",
+        "--dry-run",
+        dest="dryrun",
+        action="store_true",
+        default=False,
+        help="perform a trial run",
+    )
     return parser
 
 
 def main(args):
-    excludes = ['.git', '.gitmodules', '.DS_Store']
-    dotfiles = sorted(glob.glob('.*'))
+    excludes = [".git", ".gitmodules", ".DS_Store"]
+    dotfiles = sorted(glob.glob(".*"))
     dotfiles = sorted(list(set(dotfiles) - set(excludes)))
 
-    print('dotfiles:')
+    print("dotfiles:")
     install_dotfiles(dotfiles, args.dryrun)
-    print('')
+    print("")
 
-    binfiles = [_ for _ in glob.glob('bin/*') if is_exe(_)]
+    binfiles = [_ for _ in glob.glob("bin/*") if is_exe(_)]
     binfiles = sorted(binfiles)
 
-    print('binfiles:')
+    print("binfiles:")
     install_binfiles(binfiles, args.dryrun)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = setup_argparser()
     main(parser.parse_args())
