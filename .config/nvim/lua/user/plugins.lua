02485cb7eb3d371e1d19724276452f7dce690f16
Takeshi Kanmae <tkanmae@gmail.com>
2023-07-04 22:03:19 +0900

    vim: Allow to automatically install required tools

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -32,11 +32,38 @@ require('lazy').setup({
       ui = {
         icons = {
           package_installed = '✓',
+          package_pending = '➜',
           package_uninstalled = '✗',
-          package_pending = '⟳',
         },
       },
+      ensure_installed = {
+        'black',
+        'isort',
+        'lua-language-server',
+        'shfmt',
+        'stylua',
+        'typescript-language-server',
+      },
     },
+    config = function(_, opts)
+      require('mason').setup(opts)
+
+      local registry = require('mason-registry')
+      local function install()
+        for _, name in ipairs(opts.ensure_installed) do
+          local pkg = registry.get_package(name)
+          if not pkg.is_installed(pkg) then
+            pkg:install()
+          end
+        end
+      end
+
+      if registry.refresh then
+        registry.refresh(install)
+      else
+        install()
+      end
+    end,
   },
   {
     'jose-elias-alvarez/null-ls.nvim',
