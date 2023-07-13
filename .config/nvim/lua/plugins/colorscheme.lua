99c050dacfe14dfa4d1f86d066399922f2f5344e
Takeshi Kanmae <tkanmae@gmail.com>
2023-07-13 21:39:42 +0900

    vim: Enable more catppuccin's integrations

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -13,9 +13,28 @@ return {
     name = 'catppuccin',
     opts = {
       integrations = {
+        cmp = true,
+        gitsigns = true,
         illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { 'undercurl' },
+            hints = { 'undercurl' },
+            warning = { 'undercurl' },
+            information = { 'undercurl' },
+          },
+        },
         neotree = true,
         noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = { enabled = true },
+        treesitter = true,
         which_key = true,
       },
     },
