b09db912e4e348b248a7c1fbb8fca1464707e9f3
Takeshi Kanmae <tkanmae@gmail.com>
2023-12-20 10:39:02 +0900

    vim: Use none-ls.nvim instead of null-ls.nvim

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,9 +1,9 @@
 return {
-  'jose-elias-alvarez/null-ls.nvim',
+  'nvimtools/none-ls.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
   dependencies = { 'williamboman/mason.nvim', 'nvim-lua/plenary.nvim' },
   opts = function()
-    local null_ls = require('null-ls')
+    local none_ls = require('null-ls')
     local augroup = vim.api.nvim_create_augroup('lsp_formatting', { clear = true })
 
     return {
@@ -27,12 +27,12 @@ return {
       end,
       sources = {
         -- Python
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
+        none_ls.builtins.formatting.black,
+        none_ls.builtins.formatting.isort,
         -- Lua
-        null_ls.builtins.formatting.stylua,
+        none_ls.builtins.formatting.stylua,
         -- Text
-        null_ls.builtins.diagnostics.textlint.with({
+        none_ls.builtins.diagnostics.textlint.with({
           filetypes = { 'markdown', 'asciidoc' },
           condition = function(utils)
             return vim.fn.executable('textlint') > 0 and utils.root_has_file({ '.textlintrc' })
